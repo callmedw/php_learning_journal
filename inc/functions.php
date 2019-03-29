0301ed60ea011555dbaf01fb4980853a314d3627
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-03-29 12:32:31 -0700

    add/update journal entry

--- inc/functions.php
+++ inc/functions.php
@@ -1,1 +1,31 @@
 <?php
+
+//      journal CRUD      //
+
+// add/update journal entry
+function addJournalEntry($title, $date, $timeSpent, $learned, $resources){
+  include 'connection.php';
+
+  if ($entry_id) {
+    $sql =  'UPDATE entries SET title = ?, date = ?, time_spent = ?, learned = ?, resources = ? WHERE entry_id = ?';
+  } else {
+    $sql = 'INSERT INTO entries(title, date, time_spent, learned, resources) VALUES(?, ?, ?, ?, ?, ?)';
+  }
+
+  try {
+    $results = $db->prepare($sql);
+    $results->bindValue(1, $title, PDO::PARAM_STR);
+    $results->bindValue(2, $date, PDO::PARAM_STR);
+    $results->bindValue(3, $timeSpent, PDO::PARAM_STR);
+    $results->bindValue(4, $learned, PDO::PARAM_STR);
+    $results->bindValue(5, $resources, PDO::PARAM_STR);
+    if ($entry_id) {
+      $results->bindValue(6, $entry_id, PDO::PARAM_INT);
+    }
+    $results->execute();
+  } catch (Exception $e) {
+    echo $e->getMessage();
+    return false;
+  }
+  return true;
+}
