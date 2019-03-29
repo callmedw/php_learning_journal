621817b49a4757146855121c3a8ff12d96ff4463
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-03-29 13:49:01 -0700

    fix update placeholders for successful add_entry

--- inc/functions.php
+++ inc/functions.php
@@ -3,20 +3,20 @@
 //      journal CRUD      //
 
 // add/update journal entry
-function addJournalEntry($title, $date, $timeSpent, $learned, $resources){
+function add_journal_entry($title, $date, $time_spent, $learned, $resources, $entry_id = null){
   include 'connection.php';
 
   if ($entry_id) {
     $sql =  'UPDATE entries SET title = ?, date = ?, time_spent = ?, learned = ?, resources = ? WHERE entry_id = ?';
   } else {
-    $sql = 'INSERT INTO entries(title, date, time_spent, learned, resources) VALUES(?, ?, ?, ?, ?, ?)';
+    $sql = 'INSERT INTO entries(title, date, time_spent, learned, resources) VALUES(?, ?, ?, ?, ?)';
   }
 
   try {
     $results = $db->prepare($sql);
     $results->bindValue(1, $title, PDO::PARAM_STR);
     $results->bindValue(2, $date, PDO::PARAM_STR);
-    $results->bindValue(3, $timeSpent, PDO::PARAM_STR);
+    $results->bindValue(3, $time_spent, PDO::PARAM_STR);
     $results->bindValue(4, $learned, PDO::PARAM_STR);
     $results->bindValue(5, $resources, PDO::PARAM_STR);
     if ($entry_id) {
