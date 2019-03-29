3c59744e49918e009d5594d0b84d16633afdb613
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-03-29 13:24:03 -0700

    successful add/info message not desplaying

--- new.php
+++ new.php
@@ -1,4 +1,29 @@
-<?php include "inc/header.php" ?>
+<?php
+require 'inc/functions.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $entry_id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+  $title = trim(filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING));
+  $date = trim(filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING));
+  $time_spent = trim(filter_input(INPUT_POST, 'time-spent', FILTER_SANITIZE_STRING));
+  $learned = trim(filter_input(INPUT_POST, 'learned', FILTER_SANITIZE_STRING));
+  $resources = trim(filter_input(INPUT_POST, 'resources', FILTER_SANITIZE_STRING));
+
+  if (empty($title) || empty($date) || empty($time_spent) || empty($learned) || empty($resources)) {
+    $error_message = 'Please fill in all fields.';
+  } else {
+    if (add_journal_entry($title, $date, $time_spent, $learned, $resources, $entry_id)) {
+      $info_message = "Success! Your entry has been added.";
+      header('location:new.php');
+      exit;
+    } else {
+      $error_message = 'This entry was not successfully added.';
+    }
+  }
+}
+
+include "inc/header.php"
+?>
 
 <header>
   <div class="container">
@@ -12,17 +37,22 @@
   <div class="container">
     <div class="new-entry">
       <h2>New Entry</h2>
-      <form>
+      <form method="post" action="new.php">
         <label for="title"> Title</label>
         <input id="title" type="text" name="title"><br>
         <label for="date">Date</label>
         <input id="date" type="date" name="date"><br>
         <label for="time-spent"> Time Spent</label>
-        <input id="time-spent" type="text" name="timeSpent"><br>
-        <label for="what-i-learned">What I Learned</label>
-        <textarea id="what-i-learned" rows="5" name="whatILearned"></textarea>
-        <label for="resources-to-remember">Resources to Remember</label>
-        <textarea id="resources-to-remember" rows="5" name="ResourcesToRemember"></textarea>
+        <input id="time-spent" type="text" name="time-spent"><br>
+        <label for="learned">What I Learned</label>
+        <textarea id="learned" rows="5" name="learned"></textarea>
+        <label for="resources">Resources to Remember</label>
+        <textarea id="resources" rows="5" name="resources"></textarea>
+        <?php
+          if (!empty($entry_id)) {
+            echo "<input type='hidden' name='id' value='$entry_id' />";
+          }
+        ?>
         <input type="submit" value="Publish Entry" class="button">
         <a href="#" class="button button-secondary">Cancel</a>
       </form>
