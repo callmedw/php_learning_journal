6ab839b4f6f31b2705372642be52a3dd723000cf
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-03-29 18:41:03 -0700

    edit works, but it won't populate learned,resources, date

--- edit.php
+++ edit.php
@@ -1,4 +1,33 @@
-<?php include "inc/header.php" ?>
+<?php
+require_once 'inc/functions.php';
+
+if (isset($_GET['id'])) {
+  list($entry_id, $title, $date, $time_spent, $learned, $resources) = get_journal_entry(filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT));
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $entry_id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+  $title = trim(filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING));
+  $date = trim(filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING));
+  $time_spent = trim(filter_input(INPUT_POST, 'time-spent', FILTER_SANITIZE_STRING));
+  $learned = trim(filter_input(INPUT_POST, 'learned', FILTER_SANITIZE_STRING));
+  $resources = trim(filter_input(INPUT_POST, 'resources', FILTER_SANITIZE_STRING));
+
+  if (empty($title) || empty($date) || empty($time_spent) || empty($learned) || empty($resources)) {
+    $error_message = 'Please fill in all fields.';
+  } else {
+    if (add_journal_entry($title, $date, $time_spent, $learned, $resources, $entry_id)) {
+      $info_message = "Success! Entry Updated.";
+      header("location:detail.php?id=$entry_id");
+      exit;
+    } else {
+      $error_message = 'Whoops! Something went wrong and this entry did not update.';
+    }
+  }
+}
+
+include "inc/header.php"
+?>
 
 <header>
   <div class="container">
@@ -12,19 +41,15 @@
   <div class="container">
     <div class="edit-entry">
       <h2>Edit Entry</h2>
-      <form>
-        <label for="title"> Title</label>
-        <input id="title" type="text" name="title"><br>
-        <label for="date">Date</label>
-        <input id="date" type="date" name="date"><br>
-        <label for="time-spent"> Time Spent</label>
-        <input id="time-spent" type="text" name="timeSpent"><br>
-        <label for="what-i-learned">What I Learned</label>
-        <textarea id="what-i-learned" rows="5" name="whatILearned"></textarea>
-        <label for="resources-to-remember">Resources to Remember</label>
-        <textarea id="resources-to-remember" rows="5" name="ResourcesToRemember"></textarea>
+      <?php
+        if(isset($message)) {
+          echo "<p class='message'>$message</p>";
+        }
+      ?>
+      <form method="post" action="edit.php?id=<?php echo $entry_id; ?>">
+        <?php include "inc/journal_entry_form.php" ?>
         <input type="submit" value="Publish Entry" class="button">
-        <a href="#" class="button button-secondary">Cancel</a>
+        <a href="javascript:history.back()" class="button button-secondary">Cancel</a>
       </form>
     </div>
   </div>
